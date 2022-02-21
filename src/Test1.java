8b25b5e561e2d0edc991214280a84f976fad9300
arun <r.arunkrishna10@gmail.com>
2022-02-21 23:05:32 +0530

    first Commit

---
+++ src/Test1.java
@@ -0,0 +1,16 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String git="github";
+
+if(git.contentEquals("github")) {
+	System.out.println("success");
+}
+else {
+	System.out.println("fail");
+}
+	}
+
+}
